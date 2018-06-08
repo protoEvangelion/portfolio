9cea99a2b91c6a6c4877efa63b896d13653f78b4
protoEvangelion <ryantgarant@gmail.com>
2018-06-07 21:54:42 -0700

    Remove gatsby cache

--- gatsby/.cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/ryan/dev/notes/gatsby/src/layouts/index.js"
-  import data from "/home/ryan/dev/notes/gatsby/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
