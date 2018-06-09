b308f4e23102dfc6122405005ce9a1df0b413821
protoEvangelion <ryantgarant@gmail.com>
2018-06-09 13:52:20 -0700

    SF JS files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
 exports.onClientEntry = function() {
-	require(`es6-object-assign`).polyfill()
+  require(`es6-object-assign`).polyfill()
 }
--- src/styles/index.js
+++ src/styles/index.js
@@ -4,23 +4,23 @@ import { colors } from './colors'
 import presets from '../utils/presets'
 
 const animations = {
-	animationCurveFastOutSlowIn: `cubic-bezier(0.4, 0, 0.2, 1)`,
-	animationCurveLinearOutSlowIn: `cubic-bezier(0, 0, 0.2, 1)`,
-	animationCurveFastOutLinearIn: `cubic-bezier(0.4, 0, 1, 1)`,
-	animationCurveDefault: `cubic-bezier(0.4, 0, 0.2, 1)`,
-	animationSpeedDefault: `250ms`,
-	animationSpeedFast: `200ms`,
-	animationSpeedSlow: `300ms`,
+  animationCurveFastOutSlowIn: `cubic-bezier(0.4, 0, 0.2, 1)`,
+  animationCurveLinearOutSlowIn: `cubic-bezier(0, 0, 0.2, 1)`,
+  animationCurveFastOutLinearIn: `cubic-bezier(0.4, 0, 1, 1)`,
+  animationCurveDefault: `cubic-bezier(0.4, 0, 0.2, 1)`,
+  animationSpeedDefault: `250ms`,
+  animationSpeedFast: `200ms`,
+  animationSpeedSlow: `300ms`,
 }
 
 export default {
-	animations: animations,
-	colors: colors,
-	verticalPadding: style({
-		padding: rhythm(3 / 4),
-	}),
-	container: style({
-		maxWidth: `37rem`,
-		margin: `0 auto`,
-	}),
+  animations: animations,
+  colors: colors,
+  verticalPadding: style({
+    padding: rhythm(3 / 4),
+  }),
+  container: style({
+    maxWidth: `37rem`,
+    margin: `0 auto`,
+  }),
 }
