8d61991a128a58e27a13dd5230150bafb2c2b83e
Ryan Garant <ryantgarant@gmail.com>
2018-11-11 19:41:44 -0800

    Add jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,45 @@
 module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.tsx'],
+  coverageDirectory: '<rootDir>/coverage/',
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'jestGlobalMocks.ts',
+    '<rootDir>/src/*.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 24,
+      branches: 8,
+      functions: 7,
+      lines: 24,
+    },
   },
-  testRegex: '(/__tests__/.*\\.([tj]sx?)|(\\.|/)(test|spec))\\.([tj]sx?)$',
+  globals: {
+    __PATH_PREFIX__: '',
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true,
+      },
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '@/([^\\.]*)$': '<rootDir>/src/$1',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['node_modules', '.cache'],
-  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
-  globals: {
-    __PATH_PREFIX__: '',
-  },
-  testURL: 'http://localhost',
   setupFiles: ['<rootDir>/loadershim.js'],
   setupTestFrameworkScriptFile: require.resolve('./jest.setup.ts'),
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  testRegex: '(/__tests__/.*\\.([tj]sx?)|(\\.|/)(test|spec))\\.([tj]sx?)$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  testURL: 'http://localhost',
 }
