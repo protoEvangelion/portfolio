abf41b8efe0b147952cba4fa0ce97d96569264e5
Ryan Garant <ryantgarant@gmail.com>
2018-12-03 08:00:11 -0800

    Add contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,38 @@
 import * as React from 'react'
+import galaxy from '@/assets/galaxy.jpg'
+import { styled } from '@/style'
 
-const ContactPage = () => <h2>Projects</h2>
+const Img = styled.div`
+  background: url(${galaxy}) no-repeat center center fixed;
+  background-size: cover;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Email = styled.div`
+  background: white;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(0);
+  transition: transform 0.5s;
+  padding: 5rem;
+  max-width: 90%;
+
+  &:hover {
+    transform: translateY(-30px);
+  }
+`
+
+const ContactPage = () => (
+  <Img>
+    <Email>
+      <h4>ryantgarant@gmail.com</h4>
+    </Email>
+  </Img>
+)
 
 export default ContactPage
