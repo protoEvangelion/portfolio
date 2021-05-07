9ea4460a9bb13ea85c0f7bd061d453f6cd3fe3b0
Ryan Garant <rgarant@tenable.com>
2021-05-06 22:00:07 -0700

    fix gatsby node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,70 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+    path: `.env`,
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
-  siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `RG Portfolio`,
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
-      options: {},
+    siteMetadata: {
+        author: 'Ryan Garant',
+        title: `Ryan's Blog`,
+        description: 'Functional Programming Nerd',
+        keywords: ['tech', 'blog', 'functional programming', 'JavaScript'],
+        siteUrl: 'https://rhino.codes',
+        siteURL: 'https://rhino.codes',
+        siteImage: 'name-of-open-graphy-image.jpg', // pop an image in the static folder to use it as og:image
+        config: {
+            headerHeight: 64,
+            sideBarWidth: 240,
+            twitter: 'protoEvangelion', // no need to include the @
+            github: 'protoEvangelion',
+        },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Ryan Garant`,
-        short_name: `RG`,
-        description: `Portfolio + Blog + Fun`,
-        start_url: `/`,
-        background_color: `#141821`,
-        theme_color: `#f6ad55`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
+    plugins: [
+        {
+            resolve: `@lekoarts/gatsby-theme-cara`,
+            // See the theme's README for all available options
+            options: {},
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_ID,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Ryan Garant`,
+                short_name: `RG`,
+                description: `Portfolio + Blog + Fun`,
+                start_url: `/`,
+                background_color: `#141821`,
+                theme_color: `#f6ad55`,
+                display: `standalone`,
+                icons: [
+                    {
+                        src: `/android-chrome-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/android-chrome-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify`,
+        shouldAnalyseBundle && {
+            resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+            options: {
+                analyzerMode: `static`,
+                reportFilename: `_bundle.html`,
+                openAnalyzer: false,
+            },
+        },
+    ].filter(Boolean),
+};
---
+++ src/pages/blog/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Blog() {
+    return <div>BLOGIN</div>;
+}
+
+export default Blog;
