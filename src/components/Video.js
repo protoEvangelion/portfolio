ed3f8c60d22d153dd1a6bcb5acd4e799b23a05d1
Ryan Garant <rgarant@tenable.com>
2021-05-08 14:01:46 -0700

    add projects

---
+++ src/components/Video.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import nodegh from '../assets/videos/nodegh.webm';
+import lumin from '../assets/videos/lumin.webm';
+import liferay from '../assets/videos/liferay.webm';
+
+const vids = {
+    nodegh,
+    lumin,
+    liferay,
+};
+
+export function Video({ url }) {
+    const vid = vids[url];
+    return (
+        vid && (
+            <video autoPlay muted loop controls width="100%">
+                <source src={vid} type="video/webm" />
+            </video>
+        )
+    );
+}
