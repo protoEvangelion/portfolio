94af7b1af9c81c2c886abeebdd28820972286fb4
Ryan Garant <rgarant@tenable.com>
2021-05-10 21:58:30 -0700

    add components

---
+++ src/gatsby-plugin-theme-ui/components.js
@@ -0,0 +1,10 @@
+import PostCard from '../components/PostCard';
+import Video from '../components/Video';
+
+const components = {
+    // eslint-disable-next-line react/display-name
+    PostCard,
+    Video,
+};
+
+export default components;
