498ef06669b09c225787d1cf8a7ed32e113a24c8
rdotg <ryrycli@pm.me>
2021-10-04 11:52:41 -0700

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,67 +20,43 @@ module.exports = {
             'nodejs',
             'web',
         ],
-        siteUrl: 'https://gatsby-theme-terminal.netlify.com',
-        siteImage: 'name-of-open-graph-image.jpg', // pop an image in the static folder to use it as the og:image,
-        profileImage: 'name-of-profile-image.jpg',
+        siteUrl: 'https://artg.me',
+        siteImage: 'headshot.jpg', // pop an image in the static folder to use it as the og:image,
+        profileImage: 'headshot.jpg',
         lang: `eng`,
         config: {
             sidebarWidth: 240, // optional,
         },
     },
     plugins: [
-        // {
-        //     resolve: `@lekoarts/gatsby-theme-cara`,
-        //     // See the theme's README for all available options
-        //     options: {
-        //         mdx: false,
-        //     },
-        // },
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-mdx-embed`,
+        `gatsby-plugin-image`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                defaults: {
+                    quality: 70,
+                    formats: ['auto', 'webp', 'avif'],
+                    placeholder: 'blurred',
+                },
+            },
+        },
         {
             resolve: `@pauliescanlon/gatsby-theme-terminal`,
             options: {
-                source: [`posts`, `projects`],
+                source: [
+                    {
+                        name: 'posts',
+                        dir: 'posts',
+                    },
+                    {
+                        name: 'projects',
+                        dir: 'projects',
+                    },
+                ],
             },
         },
-        // {
-        //     resolve: `gatsby-plugin-google-analytics`,
-        //     options: {
-        //         trackingId: process.env.GOOGLE_ANALYTICS_ID,
-        //     },
-        // },
-        // {
-        //     resolve: `gatsby-plugin-manifest`,
-        //     options: {
-        //         name: `Ryan Garant`,
-        //         short_name: `RG`,
-        //         description: `Portfolio + Blog + Fun`,
-        //         start_url: `/`,
-        //         background_color: `#141821`,
-        //         theme_color: `#f6ad55`,
-        //         display: `standalone`,
-        //         icons: [
-        //             {
-        //                 src: `/android-chrome-192x192.png`,
-        //                 sizes: `192x192`,
-        //                 type: `image/png`,
-        //             },
-        //             {
-        //                 src: `/android-chrome-512x512.png`,
-        //                 sizes: `512x512`,
-        //                 type: `image/png`,
-        //             },
-        //         ],
-        //     },
-        // },
-        // `gatsby-plugin-offline`,
-        // `gatsby-plugin-netlify`,
-        // shouldAnalyseBundle && {
-        //     resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-        //     options: {
-        //         analyzerMode: `static`,
-        //         reportFilename: `_bundle.html`,
-        //         openAnalyzer: false,
-        //     },
-        // },
-    ].filter(Boolean),
+    ],
 };
